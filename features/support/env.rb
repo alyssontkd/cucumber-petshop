cacaec51b75f2661a84ded6e1d8349342432423e
Alysson Vicuna de Oliveira <alyssontkd@gmail.com>
2019-03-18 10:39:12 -0300

    [Atualizar]
    Atualizar

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,8 +16,8 @@ CUSTOM = YAML.load_file(File.dirname(__FILE__) + "/config.yml")
 
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome # Com Navegador
-  #config.default_driver = :selenium_chrome_headless # Sem navegador
+  #config.default_driver = :selenium_chrome # Com Navegador
+  config.default_driver = :selenium_chrome_headless # Sem navegador
   config.app_host = CONFIG['url_padrao']
   Capybara.default_max_wait_time = 15
 end
