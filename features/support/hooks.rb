31cba3eb6f73faa15f9cf3edf354b7c1ef4f8dcc
ALYSSON Vicuna de Oliveira <alysson.oliveira@poupex.com.br>
2019-11-13 10:40:33 -0300

    [UPDATE] Atualizando a documentação e o projeto cucumber

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,5 @@
+require "report_builder"
+
 Before() do
   visit ''
 end
@@ -8,6 +10,25 @@ After do |scenario|
   screenshot = "log/screenshots/#{nome_cenario}.png"
   page.save_screenshot(screenshot)
   embed(screenshot, 'image/png', 'Confira a evidência')
+end
+
+at_exit do
+
+  @infos = {
+      'Aplicação' => "Nome do Sistema",
+      'Ambiente' =>  "#{AMBIENTE}",
+      'Data e Hora' => DateTime.now
+  }
+
+  ReportBuilder.configure do |config|
+    config.input_path = "log/report.json"
+    config.report_path = "log/report"
+    config.report_types = [:html]
+    config.color = "indigo"
+    config.report_title = "Nome do Sistema"
+    config.additional_info = @infos
+  end
+  ReportBuilder.build_report
 end
 
 #After() do
